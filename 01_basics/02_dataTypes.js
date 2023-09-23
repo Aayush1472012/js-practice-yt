e39390bc53dce01091fc3036e5b14e0f60a726da
Aayushkumar Patel <62372257+Aayush1472012@users.noreply.github.com>
2023-09-23 09:31:50 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3+3);  // we are using nodejs, not browser
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => default value of variable
+// NaN
+
+
+// object
+
+let name = "Aayush";
+let age = 21;
+let isLoggedIn = true;
+let state;
+
+console.log(name, typeof(name));
+console.log(age, typeof(age));
+console.log(isLoggedIn, typeof(isLoggedIn));
+console.log(state, typeof(state));
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // object
+console.log(typeof NaN);    // number
