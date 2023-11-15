cd3d2a834db1fa914929fa196e8faa22f2a62d27
Aayushkumar Patel <62372257+Aayush1472012@users.noreply.github.com>
2023-11-15 16:45:48 +0000

    while & do-while loop in js

---
+++ 05_iterations/02_while-&-do-while-loop.js
@@ -0,0 +1,23 @@
+// while loop
+let i = 0;
+while(i < 10){
+    console.log(i);
+    i = i+2;
+}
+
+let marvel_heros = ["Dr.Strange", "Spiderman", "Ironman", "Hulk"];
+let arr_index = 0;
+while(arr_index < marvel_heros.length){
+    console.log(marvel_heros[arr_index]);
+    arr_index++;
+}
+
+
+
+// do-while loop
+let score = 10;
+
+do{
+    console.log(score);
+    score++;
+}while(score < 10);
