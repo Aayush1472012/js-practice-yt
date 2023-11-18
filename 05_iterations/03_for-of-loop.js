92d8570957dfdf9a694acbc005624cb127b552fe
Aayushkumar Patel <62372257+Aayush1472012@users.noreply.github.com>
2023-11-18 08:46:18 +0000

    for of loop in js

---
+++ 05_iterations/03_for-of-loop.js
@@ -0,0 +1,50 @@
+// array of strings -> ["", "", ""]
+// array of objects -> [{}, {}, {}]
+
+// we can iterate over 'Array', 'String', 'Map' using for-of loop, but we cannot iterate over object using for-of loop.
+
+
+// Array
+const myArr = [1, 2, 3, 4, 5];
+
+for(const element of myArr){
+    console.log(element);
+}
+
+
+// String
+const greeting = "Hello World!!!";
+
+for(const greet of greeting){
+    console.log(greet);
+}
+
+
+// Map
+const myMap = new Map();
+myMap.set('IN', 'India');
+myMap.set('UK', 'United Kingdom');
+myMap.set('JP', 'Japan');
+myMap.set('AUS', 'Australia');
+
+for(const element of myMap){
+    console.log(element);
+}
+
+for(const [key, value] of myMap){
+    // used array destructuring
+    console.log(`${key}:- ${value}`);
+}
+
+
+// Object
+const myObj = {
+    hero1: "Dr.Strange",
+    hero2: "Ironman",
+    hero3: "Hulk"
+};
+
+// "TypeError: myObj is not iterable"
+// for(const element of myObj){
+//     console.log(element);
+// }
